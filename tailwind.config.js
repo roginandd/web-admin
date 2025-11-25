8cd80a7b6174bdd4c15c04dd4bfecf4b7da3598e
roginand <roginandv@gmail.com>
2025-11-25 14:43:44 +0800

    feat(components): imported necessary shadcn

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,13 @@
+import tailwindAnimate from "tailwindcss-animate";
+
 /** @type {import('tailwindcss').Config} */
 export default {
+  darkMode: ["class"],
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      // your theme settings...
+    },
   },
-  plugins: [],
+  plugins: [tailwindAnimate],
 };
